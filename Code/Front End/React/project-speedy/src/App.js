745288c66029034df240a15810048039c464e093
Matt Turner <tmatt95@gmail.com>
2021-02-15 20:50:17 +0000

    Tidying up code

--- Code/Front End/React/project-speedy/src/App.js
+++ Code/Front End/React/project-speedy/src/App.js
@@ -60,7 +60,6 @@ export default function App() {
           <button onClick={() => setCount(count + 1)}>
             Click me
           </button>
-
             <Router>
               <div>
                 <ul>

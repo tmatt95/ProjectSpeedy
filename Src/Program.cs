bc67f54cce18a5a71eaa48c130282d5d6d15d6db
Matt Turner <tmatt95@gmail.com>
2021-03-13 18:56:32 +0000

    Getting build to build

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,22 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ProjectSpeedy
 {
-    protected class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        protected static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        protected static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {

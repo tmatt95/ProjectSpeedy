6eb650c0618c3c7a495423cde52d443c95cfa0b2
Matt Turner <tmatt95@gmail.com>
2021-03-13 14:53:55 +0000

    Bet new class added

--- Src/Models/Bet/BetNew.cs
+++ Src/Models/Bet/BetNew.cs
@@ -1,4 +1,23 @@
-public class BetNew
+namespace ProjectSpeedy
 {
-   // Fields, properties, methods and events go here....
+   /**
+   * Used to create a new bet.
+   **/
+   class BetNew
+   {
+      /**
+      * Gets or sets the name of the bet.
+      **/
+      public string Name {get; set;}
+
+      /**
+      * Gets or sets the description of the bet.
+      **/
+      public string Description {get; set;}
+
+      /**
+      * Gets or sets the measures of success the bet will be judged against.
+      **/
+      public string MeasuresOfSuccess {get; set;}
+   }
 }

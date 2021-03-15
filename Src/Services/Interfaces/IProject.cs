e694f009bc6aa9c5c9f2d4cf7d43cd23f91b0b12
Matt Turner <tmatt95@gmail.com>
2021-03-15 08:25:38 +0000

    Fixing issues with project service

--- Src/Services/Interfaces/IProject.cs
+++ Src/Services/Interfaces/IProject.cs
@@ -5,10 +5,29 @@ namespace ProjectSpeedy.Services
     /// </summary>
     interface IProject
     {
-        bool Create();
+        /// <summary>
+        /// Every bet needs to be linked to a problem and a problem needs to be linked to a project. 
+        /// This lets the user create a new project. Very little information is required when creating 
+        /// a project. This can be filled in at a later date (as it might not be known at the time).
+        /// </summary>
+        /// <param name="form">Form containing information on the new project.</param>
+        /// <returns>If the new project was added successfully.</returns>
+        bool Create(ProjectSpeedy.Project.ProjectUpdate form);
 
-        bool update();
+        /// <summary>
+        /// The create project action has been designed to be simple and contain the minimum number of fields. 
+        /// The user will need to have the ability to update the project to add missing information.
+        /// </summary>
+        /// <param name="projectId">Project identifier</param>
+        /// <param name="form">Form containing information on the new project.</param>
+        /// <returns>If the project has been updated successfully.</returns>
+        bool Update(string projectId, ProjectSpeedy.Project.ProjectUpdate form);
 
-        bool Delete();
+        /// <summary>
+        /// This action will delete the project and linked problems / bets.
+        /// </summary>
+        /// <param name="projectId">Project identifier</param>
+        /// <returns>If the project has been deleted successfully.</returns>
+        bool Delete(string projectId);
     }
 }
--- Src/Services/Project.cs
+++ Src/Services/Project.cs
@@ -1,9 +1,28 @@
+using ProjectSpeedy.Project;
+
 namespace ProjectSpeedy.Services
 {
     /// <summary>
     /// All project related services.
     /// </summary>
-    interface Project : IProject
+    public class Project : IProject
     {
+        /// <inheritdoc />
+        public bool Create(ProjectUpdate form)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public bool Delete(string projectId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public bool Update(string projectId, ProjectUpdate form)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }

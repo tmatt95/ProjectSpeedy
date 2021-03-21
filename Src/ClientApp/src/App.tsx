51160f09cac0ce72828853ad5e4d98c0a752ced5
Matt Turner <tmatt95@gmail.com>
2021-03-21 09:52:26 +0000

    fixing empty object pattern

--- Src/ClientApp/src/App.tsx
+++ Src/ClientApp/src/App.tsx
@@ -3,15 +3,16 @@ import './App.css';
 import { Projects } from './Pages/Projects';
 import { Project } from './Pages/Project';
 import { Problem } from './Pages/Problem';
-import { IGlobalMessage  } from './Interfaces/Components';
+import { IGlobalMessage } from './Interfaces/Components';
 import { BreadCrumbItem, BreadCrumbs } from './Components/BreadCrumbs';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-} from "react-router-dom";
+import
+  {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+  } from "react-router-dom";
 
 
 /**
@@ -19,25 +20,28 @@ import {
  * @param param0 
  * @returns 
  */
-function GlobalMessage({ globalMessage, message }: { globalMessage: ({}: IGlobalMessage) => void, message: IGlobalMessage })
+function GlobalMessage({ globalMessage, message }: { globalMessage: (arg0: IGlobalMessage) => void, message: IGlobalMessage })
 {
-  if (message !== null) {
+  if (message !== null)
+  {
     let classes = `alert alert-dismissible fade show ${message.class}`;
     return <>
-    <div className={classes} role="alert">
-      {message.message}
-      <button type="button" className="btn-close" onClick={() => globalMessage({message:"", class:"d-none"})} aria-label="Close"></button>
-    </div>
+      <div className={classes} role="alert">
+        {message.message}
+        <button type="button" className="btn-close" onClick={() => globalMessage({ message: "", class: "d-none" })} aria-label="Close"></button>
+      </div>
     </>;
   }
-  else {
+  else
+  {
     return <></>
   }
 }
 
-export default function App() {
+export default function App()
+{
   // Used to store page messages.
-  const [globalMessage, setGlobalMessage]: [IGlobalMessage, Dispatch<IGlobalMessage>] = useState({message: "", class: "d-none"});
+  const [globalMessage, setGlobalMessage]: [IGlobalMessage, Dispatch<IGlobalMessage>] = useState({ message: "", class: "d-none" });
 
   // Used to store the data used to generate the breadcrumbs for the page.
   var initalCrumbs: BreadCrumbItem[] = [{ text: "Projects", address: "/", isLast: true }];
@@ -81,10 +85,10 @@ export default function App() {
         <div className="container">
           <Switch>
             <Route path="/project">
-              <ProjectSection breadCrumbs={breadCrumbs} setBreadCrumbs={(crumbs: BreadCrumbItem[]) =>{setBreadCrumbs(crumbs)}} globalMessage={(alertMessage: IGlobalMessage) => setGlobalMessage(alertMessage)} />
+              <ProjectSection breadCrumbs={breadCrumbs} setBreadCrumbs={(crumbs: BreadCrumbItem[]) => { setBreadCrumbs(crumbs) }} globalMessage={(alertMessage: IGlobalMessage) => setGlobalMessage(alertMessage)} />
             </Route>
             <Route path="/">
-              <Projects breadCrumbs={breadCrumbs} setBreadCrumbs={(crumbs: BreadCrumbItem[]) =>{setBreadCrumbs(crumbs)}}  globalMessage={(alertMessage: IGlobalMessage) => setGlobalMessage(alertMessage)} />
+              <Projects breadCrumbs={breadCrumbs} setBreadCrumbs={(crumbs: BreadCrumbItem[]) => { setBreadCrumbs(crumbs) }} globalMessage={(alertMessage: IGlobalMessage) => setGlobalMessage(alertMessage)} />
             </Route>
           </Switch>
         </div>
@@ -97,19 +101,20 @@ export default function App() {
  * Contains links to the project section of the application.
  * @param {*} props Properties sent into the section.
  */
-function ProjectSection({ setBreadCrumbs, breadCrumbs, globalMessage }: {setBreadCrumbs: (crumbs: BreadCrumbItem[]) => void, breadCrumbs: BreadCrumbItem[], globalMessage: (alertMessage: IGlobalMessage) => void}) {
+function ProjectSection({ setBreadCrumbs, breadCrumbs, globalMessage }: { setBreadCrumbs: (crumbs: BreadCrumbItem[]) => void, breadCrumbs: BreadCrumbItem[], globalMessage: (alertMessage: IGlobalMessage) => void })
+{
   let match = useRouteMatch();
   return (
     <>
       <Switch>
-      <Route path={`${match.path}/:projectId/:problemId/:betId`}>
+        <Route path={`${match.path}/:projectId/:problemId/:betId`}>
           <h1>Bet</h1>
         </Route>
         <Route path={`${match.path}/:projectId/:problemId`}>
-          <Problem breadCrumbs={breadCrumbs} setBreadCrumbs={(crumbs: BreadCrumbItem[]) => {setBreadCrumbs(crumbs)}} globalMessage={globalMessage} />
+          <Problem breadCrumbs={breadCrumbs} setBreadCrumbs={(crumbs: BreadCrumbItem[]) => { setBreadCrumbs(crumbs) }} globalMessage={globalMessage} />
         </Route>
         <Route path={`${match.path}/:projectId`}>
-          <Project breadCrumbs={breadCrumbs} setBreadCrumbs={(crumbs: BreadCrumbItem[]) => {setBreadCrumbs(crumbs)}} globalMessage={globalMessage} />
+          <Project breadCrumbs={breadCrumbs} setBreadCrumbs={(crumbs: BreadCrumbItem[]) => { setBreadCrumbs(crumbs) }} globalMessage={globalMessage} />
         </Route>
         <Route path={match.path}>
           <h3>Project Id not supplied</h3>

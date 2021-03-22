438161dedd692c97e9a6981c18314435d3d7d25e
Matt Turner <tmatt95@gmail.com>
2021-03-22 14:42:29 +0000

    adding test folder

--- Src/ClientApp/src/__tests__/App.js
+++ Src/ClientApp/src/__tests__/App.js
@@ -1,10 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './../App';
 
-describe(`The overall application component`, () =>
-{
-  it('should render without errror', () =>
-  {
+describe(`The overall application component`, () => {
+  it('should render without errror', () => {
     render(<App />);
     const linkElement = screen.getByText(/Project Speedy/i);
     expect(linkElement).toBeInTheDocument();

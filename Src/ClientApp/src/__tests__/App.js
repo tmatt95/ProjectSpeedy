f9611191997ee025d98770b9170bcc952bae18f0
Matt Turner <tmatt95@gmail.com>
2021-03-22 22:09:48 +0000

    tweaking app.js

--- Src/ClientApp/src/__tests__/App.js
+++ Src/ClientApp/src/__tests__/App.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './../App';
 
 describe(`The overall application component`, () => {
-  it('should render without errror', () => {
+  it('should render page without errror', () => {
     render(<App />);
     const linkElement = screen.getByText(/Project Speedy/i);
     expect(linkElement).toBeInTheDocument();

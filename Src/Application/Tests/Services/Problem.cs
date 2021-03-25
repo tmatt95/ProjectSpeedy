fc01f423b52cf1924803fffe54ab4ef051e1bcd9
Matt Turner <tmatt95@gmail.com>
2021-03-25 17:04:03 +0000

    Adding get problem unit test

--- Src/Application/Tests/Services/Problem.cs
+++ Src/Application/Tests/Services/Problem.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
@@ -44,6 +47,52 @@ namespace Tests.Services
 
             // Assert
             Assert.AreEqual(test, false);
+        }
+
+        // Get project
+        [Test]
+        public async System.Threading.Tasks.Task GetProblem()
+        {
+            // Arrange
+            var mockTest = new Mock<ProjectSpeedy.Services.IServiceBase>();
+            var problemService = new ProjectSpeedy.Services.Problem(mockTest.Object);
+
+            // Creates the fake response
+            HttpResponseMessage responseView = new HttpResponseMessage();
+            string contentView = JsonSerializer.Serialize(new ProjectSpeedy.Models.CouchDb.View.ViewResult(){
+                rows = new List<ProjectSpeedy.Models.CouchDb.View.ListItem>(){
+                    new ProjectSpeedy.Models.CouchDb.View.ListItem(){
+                        id="recordId",
+                        key="recordKey",
+                        value = new ProjectSpeedy.Models.CouchDb.View.ListItemValue(){
+                            name = "Bet Name",
+                            id="bet:BetId",
+                            status="Not Started"
+                        }
+                    }
+                }
+            });
+            responseView.Content = new StringContent(contentView);
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            string content = JsonSerializer.Serialize(new ProjectSpeedy.Models.Project.Project(){
+                Name="Problem Name",
+                Description="Problem Description"
+            });
+            response.Content = new StringContent(content);
+            mockTest.Setup(d => d.DocumentGet(It.IsAny<string>()))
+                .Returns(Task.FromResult(response.Content));
+
+            mockTest.Setup(d => d.ViewGet(It.IsAny<string>(),It.IsAny<string>(),It.IsAny<string>(),It.IsAny<string>(),It.IsAny<string>()))
+                .Returns(Task.FromResult(responseView.Content));
+
+            // Act
+            var test = await problemService.GetAsync("ProjectId","ProblemId");
+
+            // Assert
+            Assert.IsInstanceOf<ProjectSpeedy.Models.Problem.Problem>(test);
+            Assert.IsNotNull(test);
+            Assert.AreEqual("Problem Name", test.Name);
         }
     }
 }

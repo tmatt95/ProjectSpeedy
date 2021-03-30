474193ccc04a28f494ceca366e9e54e1bdd23a70
Matt Turner <tmatt95@gmail.com>
2021-03-30 22:44:56 +0100

    First service test added

---
+++ Src/Application/Code/ClientApp/src/__tests__/Services/ProjectService.ts
@@ -0,0 +1,17 @@
+import { CardItem } from '../../Components/CardGrid';
+import { IProject } from '../../Interfaces/IPage';
+import { ProjectService } from '../../Services/ProjectService';
+
+describe(`The project new form component`,() => {
+  it('can get a project', async () =>
+  {
+    jest.spyOn(global, "fetch").mockImplementation((input: RequestInfo, init?: RequestInit | undefined) =>
+    {
+      let project: IProject = { description: "description", name: "ProjectName", problems: [], isLoaded: true };
+      return Promise.resolve(new Response(JSON.stringify(project)));
+    });
+    console.log((await ProjectService.GetAll()));
+    expect((await ProjectService.Get("ProjectId")).name).toBe("ProjectName");
+  });
+});
+

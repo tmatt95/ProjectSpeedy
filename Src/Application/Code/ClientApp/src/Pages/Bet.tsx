c282aa83ba84adc9c0cd79be00cc38385e389822
Matt Turner <tmatt95@gmail.com>
2021-04-01 20:04:56 +0100

    trying to lower cognitive complexity

--- Src/Application/Code/ClientApp/src/Pages/Bet.tsx
+++ Src/Application/Code/ClientApp/src/Pages/Bet.tsx
@@ -68,7 +68,7 @@ export function Bet(pageProps: IPage)
     {
         return <>
             <div className="mb-3 mt-2">
-            <label htmlFor="comment">Add a new comment</label>
+                <label htmlFor="comment">Add a new comment</label>
                 <textarea
                     id="comment"
                     name="comment"
@@ -90,52 +90,50 @@ export function Bet(pageProps: IPage)
             return <></>;
         }
 
-        if (bet.status === "Created")
+        switch (bet.status)
         {
-            return <>
-                <nav className="mt-3">
-                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button className="nav-link active" id="nav-comments-tab" data-bs-toggle="tab" data-bs-target="#nav-comments" type="button" role="tab" aria-controls="nav-comments" aria-selected="true">Comments</button>
+            case "Created": {
+                return <>
+                    <nav className="mt-3">
+                        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                            <button className="nav-link active" id="nav-comments-tab" data-bs-toggle="tab" data-bs-target="#nav-comments" type="button" role="tab" aria-controls="nav-comments" aria-selected="true">Comments</button>
+                        </div>
+                    </nav>
+                    <div className="tab-content" id="nav-tabContent">
+                        <div className="tab-pane fade show active" id="nav-comments" role="tabpanel" aria-labelledby="nav-comments-tab">{TabComments()}</div>
                     </div>
-                </nav>
-                <div className="tab-content" id="nav-tabContent">
-                    <div className="tab-pane fade show active" id="nav-comments" role="tabpanel" aria-labelledby="nav-comments-tab">{TabComments()}</div>
-                </div>
-            </>
-        }
-
-        if (bet.status === "In Progress")
-        {
-            return <>
-                <nav className="mt-3">
-                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button className="nav-link active" id="nav-comments-tab" data-bs-toggle="tab" data-bs-target="#nav-comments" type="button" role="tab" aria-controls="nav-comments" aria-selected="true">Comments</button>
-                        <button className="nav-link" id="nav-feedback-tab" data-bs-toggle="tab" data-bs-target="#nav-feedback" type="button" role="tab" aria-controls="nav-feedback" aria-selected="false">Feedback</button>
+                </>
+            }
+            case "In Progress": {
+                return <>
+                    <nav className="mt-3">
+                        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                            <button className="nav-link active" id="nav-comments-tab" data-bs-toggle="tab" data-bs-target="#nav-comments" type="button" role="tab" aria-controls="nav-comments" aria-selected="true">Comments</button>
+                            <button className="nav-link" id="nav-feedback-tab" data-bs-toggle="tab" data-bs-target="#nav-feedback" type="button" role="tab" aria-controls="nav-feedback" aria-selected="false">Feedback</button>
+                        </div>
+                    </nav>
+                    <div className="tab-content" id="nav-tabContent">
+                        <div className="tab-pane fade show active" id="nav-comments" role="tabpanel" aria-labelledby="nav-comments-tab">{TabComments()}</div>
+                        <div className="tab-pane fade" id="nav-feedback" role="tabpanel" aria-labelledby="nav-feedback-tab">...</div>
                     </div>
-                </nav>
-                <div className="tab-content" id="nav-tabContent">
-                    <div className="tab-pane fade show active" id="nav-comments" role="tabpanel" aria-labelledby="nav-comments-tab">{TabComments()}</div>
-                    <div className="tab-pane fade" id="nav-feedback" role="tabpanel" aria-labelledby="nav-feedback-tab">...</div>
-                </div>
-            </>
-        }
-
-        if (bet.status === "Finished")
-        {
-            return <>
-                <nav className="mt-3">
-                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button className="nav-link active" id="nav-comments-tab" data-bs-toggle="tab" data-bs-target="#nav-comments" type="button" role="tab" aria-controls="nav-comments" aria-selected="true">Comments</button>
-                        <button className="nav-link" id="nav-feedback-tab" data-bs-toggle="tab" data-bs-target="#nav-feedback" type="button" role="tab" aria-controls="nav-feedback" aria-selected="false">Feedback</button>
-                        <button className="nav-link" id="nav-outcomes-tab" data-bs-toggle="tab" data-bs-target="#nav-outcomes" type="button" role="tab" aria-controls="nav-outcomes" aria-selected="false">Outcomes</button>
+                </>
+            }
+            case "Finished": {
+                return <>
+                    <nav className="mt-3">
+                        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                            <button className="nav-link active" id="nav-comments-tab" data-bs-toggle="tab" data-bs-target="#nav-comments" type="button" role="tab" aria-controls="nav-comments" aria-selected="true">Comments</button>
+                            <button className="nav-link" id="nav-feedback-tab" data-bs-toggle="tab" data-bs-target="#nav-feedback" type="button" role="tab" aria-controls="nav-feedback" aria-selected="false">Feedback</button>
+                            <button className="nav-link" id="nav-outcomes-tab" data-bs-toggle="tab" data-bs-target="#nav-outcomes" type="button" role="tab" aria-controls="nav-outcomes" aria-selected="false">Outcomes</button>
+                        </div>
+                    </nav>
+                    <div className="tab-content" id="nav-tabContent">
+                        <div className="tab-pane fade show active" id="nav-comments" role="tabpanel" aria-labelledby="nav-comments-tab">{TabComments()}</div>
+                        <div className="tab-pane fade" id="nav-feedback" role="tabpanel" aria-labelledby="nav-feedback-tab">...</div>
+                        <div className="tab-pane fade" id="nav-outcomes" role="tabpanel" aria-labelledby="nav-outcomes-tab">...</div>
                     </div>
-                </nav>
-                <div className="tab-content" id="nav-tabContent">
-                    <div className="tab-pane fade show active" id="nav-comments" role="tabpanel" aria-labelledby="nav-comments-tab">{TabComments()}</div>
-                    <div className="tab-pane fade" id="nav-feedback" role="tabpanel" aria-labelledby="nav-feedback-tab">...</div>
-                    <div className="tab-pane fade" id="nav-outcomes" role="tabpanel" aria-labelledby="nav-outcomes-tab">...</div>
-                </div>
-            </>
+                </>
+            }
         }
     }
 
@@ -329,7 +327,7 @@ export function Bet(pageProps: IPage)
         <div className="mb-3">
             <h2>Start Bet</h2>
             <p>When your ready to begin work on the bet click start. The time will start counting down and the bet will become active. Good luck!</p>
-            <button className="btn btn-primary">Start bet</button>
+            <button className="btn btn-primary" onClick={() => { alert("test"); }}>Start bet</button>
         </div>
 
         {GetCommentsFeedbackOutcomes()}

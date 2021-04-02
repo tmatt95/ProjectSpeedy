95f226b05f42d109beb9ddc967d0cf93fc9085a7
Matt Turner <tmatt95@gmail.com>
2021-04-02 12:49:45 +0100

    Covering all situations for tabs

--- Src/Application/Code/ClientApp/src/__tests__/Components/Bet/BetTabs.tsx
+++ Src/Application/Code/ClientApp/src/__tests__/Components/Bet/BetTabs.tsx
@@ -26,6 +26,21 @@ afterEach(() =>
 
 describe(`The bet tabs component`, () =>
 {
+  it("renders nothing when bet not loaded", () =>
+  {
+    let dummyBet: IBet = { name: "", description: "", status: "", isLoaded: false, timeCurrent: 0, timeTotal: 0, successCriteria: "" } as IBet;
+    act(() =>
+    {
+      render(<BetTabs bet={dummyBet} />, container);
+    });
+
+    expect(container).not.toBeNull();
+    if (container !== null)
+    {
+      expect(container.innerHTML).toBe("");
+    }
+  });
+
   it("renders correctly when status is created", () =>
   {
     let dummyBet: IBet = { name: "Bet Name", description: "Bet description", status: "Created", isLoaded: true, timeCurrent: 0, timeTotal: 0, successCriteria: "" } as IBet;
@@ -68,6 +83,21 @@ describe(`The bet tabs component`, () =>
     if (container !== null)
     {
       expect(container.getElementsByClassName("nav-link").length).toBe(3);
+    }
+  });
+
+  it("renders nothing when it gets a status it is not expecting", () =>
+  {
+    let dummyBet: IBet = { name: "", description: "", status: "unknown", isLoaded: true, timeCurrent: 0, timeTotal: 0, successCriteria: "" } as IBet;
+    act(() =>
+    {
+      render(<BetTabs bet={dummyBet} />, container);
+    });
+
+    expect(container).not.toBeNull();
+    if (container !== null)
+    {
+      expect(container.innerHTML).toBe("");
     }
   });
 });
